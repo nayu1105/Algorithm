c389d3ee815348e262540cdd8923a6043a7e970b
nayu99 <nayu99@DESKTOP-ELFTQ4O>
2022-07-21 23:11:43 +0900

    Initial_commit

--- Algoritm_study/src/ssafy/al/july/game_devleopment.java
+++ Algoritm_study/src/ssafy/al/july/game_devleopment.java
@@ -3,7 +3,7 @@ package ssafy.al.july;
 public class game_devleopment {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("Hello World!");
 	}
 
 }
