ed9eff33ed999ca9ea3dc89cfbc4aa607912c1cc
nayu <nyj3230@naver.com>
2022-08-04 17:41:49 +0900

    [D4] Title: [S/W 문제해결 기본] 4일차 - 괄호 짝짓기, Time: 99 ms, Memory: 18,612 KB -BaekjoonHub

---
+++ SWEA/D4/1218. ［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기/［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Solution {
+	static Stack<Character> stack = new Stack<>();
+	static int ans = 1;
+
+	public static void main(String[] args) throws Exception {
+		//System.setIn(new FileInputStream("input.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		for (int t = 1; t <= 10; t++) {
+			int n = Integer.parseInt(br.readLine());
+			String s = br.readLine();
+			stack.clear();
+			ans = 1;
+
+			for (int j = 0; j < n; j++) {
+				char c = s.charAt(j);
+				if (c == '(' || c == '[' || c == '{' || c == '<') {
+					stack.push(c);
+				} else {
+					if (!stack.isEmpty()) {
+						char top = stack.pop();
+						if ((top == '(' && c == ')') || (top == '{' && c == '}') || (top == '[' && c == ']')
+								|| (top == '<' && c == '>')) {
+							continue;
+						} else {
+							ans = 0;
+							break;
+						}
+					} else {
+						ans = 0;
+						break;
+					}
+
+				}
+			}
+
+			if (!stack.isEmpty())
+				ans = 0;
+
+			System.out.println("#" + t + " " + ans);
+
+		}
+
+	}
+}
