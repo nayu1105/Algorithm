d3e9e8dadadc0a27424d1c2237c44990500d9539
nayu <nyj3230@naver.com>
2022-08-08 18:04:09 +0900

    [D3] Title: 한빈이와 Spot Mart, Time: 189 ms, Memory: 31,928 KB -BaekjoonHub

--- SWEA/D3/9229. 한빈이와 Spot Mart/한빈이와 Spot Mart.java
+++ SWEA/D3/9229. 한빈이와 Spot Mart/한빈이와 Spot Mart.java
@@ -5,7 +5,6 @@ import java.util.Collections;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
-
 public class Solution {
 	static int T, N, M;
 	static Integer[] arr;
@@ -23,25 +22,26 @@ public class Solution {
 			for (int i = 0; i < N; i++) {
 				arr[i] = Integer.parseInt(st.nextToken());
 			}
-			Arrays.sort(arr, Collections.reverseOrder());
+			Arrays.sort(arr); // sort
 
 			for (int i = 0; i < N; i++) {
-				for (int j = i+1; j < N; j++) {
+				for (int j = i + 1; j < N; j++) {
 					temp = arr[i] + arr[j];
-
-					if (weight < temp && temp <= M)
-						weight = temp;
-
-					if (weight == M) {
-						break;
-					}
+					if (temp <= M) {
+						if (weight < temp) // 계산한 값이 현재 무게보다 크면 바꾸기
+							weight = temp;
+						if (weight == M) { // 무게가 M과 같아지면 최댓값이기에 이중 포문 break;
+							break;
+						}
+					} else // 정렬을 했기에 두 숫자의 합이 M보다 커지면 그 이후의 합도 M보다 크기에 들고갈 수 없음.
+						break; //이중 포문 중 바깥 for문을 돌기 위해 나가기
 
 				}
-				if (temp == M) {
+				if (temp == M) {// 무게가 M과 같아지면 최댓값이기에 이중 포문 break;
 					break;
 				}
 			}
-			
+
 			System.out.println("#" + t + " " + weight);
 		}
 
