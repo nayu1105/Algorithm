8b2982393d9e816678637a07c76d133e572ca7b6
nayu <nyj3230@naver.com>
2022-08-18 23:22:12 +0900

    [Silver I] Title: 곱셈, Time: 80 ms, Memory: 11516 KB -BaekjoonHub

---
+++ 백준/Silver/1629. 곱셈/곱셈.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int a, b, c;
+		a = Integer.parseInt(st.nextToken());
+		b = Integer.parseInt(st.nextToken());
+		c = Integer.parseInt(st.nextToken());
+
+		long ans = pow(a, b, c);
+
+		System.out.println(ans);
+	}
+
+	static long pow(int a, int b, int c) {
+		if (b == 0)
+			return 1;
+
+		long n = pow(a, b / 2, c);
+		if (b % 2 == 0) // 짝수면 그냥 n*n
+			return n * n % c;
+		else // 홀수면 한번 더 곱해줘야함
+			return (n * n % c) * a % c;
+	}
+}
