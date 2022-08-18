9a52037b2228239e214f1c9789b103ece2b47761
nayu <nyj3230@naver.com>
2022-08-18 23:38:58 +0900

    [Gold II] Title: 버블 소트, Time: 976 ms, Memory: 71696 KB -BaekjoonHub

---
+++ 백준/Gold/1377. 버블 소트/버블 소트.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Main {
+	static int N, max = Integer.MIN_VALUE;
+	static ArrayList<Node> arr = new ArrayList<>();
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+
+		for (int i = 0; i < N; i++) {
+			int n = Integer.parseInt(br.readLine());
+			arr.add(new Node(n, i));
+		}
+
+		Collections.sort(arr, (o1, o2) -> o1.num - o2.num);
+
+		for (int i = 0; i < N; i++) {
+			max = Math.max(max, arr.get(i).idx - i);
+		}
+
+		System.out.println(max + 1);
+
+	}
+
+	static class Node {
+		int num, idx;
+
+		public Node(int num, int idx) {
+			super();
+			this.num = num;
+			this.idx = idx;
+		}
+
+	}
+}
