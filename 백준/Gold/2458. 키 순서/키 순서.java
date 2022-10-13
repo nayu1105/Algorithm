8bf4ed73be47d17b795278f166e85da23a4ab31d
nayu <nyj3230@naver.com>
2022-10-13 23:25:48 +0900

    [Gold IV] Title: 키 순서, Time: 516 ms, Memory: 37500 KB -BaekjoonHub

---
+++ 백준/Gold/2458. 키 순서/키 순서.java
@@ -0,0 +1,72 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int N, M;
+	static int[][] height;
+	static ArrayList<ArrayList<Integer>> list;
+	static boolean[] visit; // dfs 쓸 visit
+	static int cnt;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
+		height = new int[N + 1][2]; // 0 dummy
+		// 키 : 자기보다 작은 사람 수[0] 자기보다 큰 사람 수 [1]
+
+		list = new ArrayList<>();
+		for (int i = 0; i <= N; i++) {
+			list.add(new ArrayList<>());
+		}
+		// 인접 리스트 초기화. 0은 dummy
+
+		for (int i = 1; i <= M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+
+			list.get(a).add(b);
+		}
+
+		for (int i = 1; i <= N; i++) {
+			cnt = 0;
+			visit = new boolean[N + 1]; // 0 dummy
+			dfs(i);
+			height[i][1] = cnt; // dfs 하며 얻은 부모 수
+		}
+
+		int ans = 0;
+		for (int i = 1; i <= N; i++) {
+			int sum = 0;
+			for (int j = 0; j < 2; j++) {
+				sum += height[i][j];
+			}
+			if (sum == N - 1)
+				ans++;
+		}
+
+		System.out.println(ans);
+
+	}
+
+	private static void dfs(int idx) {
+		int size = list.get(idx).size();
+		for (int i = 0; i < size; i++) {
+			int n = list.get(idx).get(i);
+
+			if (visit[n])
+				continue; // 이미 방문한 노드
+
+			cnt++; // 부모 수 추가
+			height[n][0]++; // n의 자식수 추가
+			visit[n] = true;
+			dfs(n);
+
+		}
+	}
+}
