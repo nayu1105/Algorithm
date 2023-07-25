c0d5be325dccc309a7047ac651300923d0bc493d
nayu <nyj3230@naver.com>
2023-07-25 17:44:23 +0900

    [level 3] Title: 기지국 설치, Time: 1.13 ms, Memory: 53.1 MB -BaekjoonHub

--- 프로그래머스/lv3/12979. 기지국 설치/기지국 설치.java
+++ 프로그래머스/lv3/12979. 기지국 설치/기지국 설치.java
@@ -1,9 +1,5 @@
 class Solution {
     public int solution(int n, int[] stations, int w) {
-        // n = 200000000; 
-        // stations = new int[] {100000000} ;
-        // w= 5;
-        
         int answer = 0;
 
         for(int i = 0; i < stations.length; i++){
@@ -14,23 +10,19 @@ class Solution {
                 start = 1;
                 end = stations[i]-w;
                 answer += getNeedStations(end-start, w);
-                // System.out.print(answer);
             }
             
             if(i==stations.length-1){
                 start = stations[i]+w;
                 end = n;
                 answer += getNeedStations(end-start, w);
-                // System.out.print(answer);
             }
             else{
                 start = stations[i]+w+1;
                 end = stations[i+1]-w;
                 answer += getNeedStations(end-start, w);
-               // System.out.print(answer);
             }            
         }
-        // System.out.print("answer" + answer);
         return answer;
     }
     
