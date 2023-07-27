dd320cd688e0beabaecea7e6a8deb147033468db
nayu <nyj3230@naver.com>
2023-07-27 23:48:44 +0900

    [level 3] Title: 섬 연결하기, Time: 0.72 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/lv3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/lv3/42861. 섬 연결하기/섬 연결하기.java
@@ -1,59 +1,42 @@
 import java.util.*;
 class Solution {
-    static int[] parents;
     public int solution(int n, int[][] costs) {
 		boolean[] visited = new boolean[n];
-		parents = new int[n];
 
 		Arrays.sort(costs, (c1, c2) -> {
 			return c1[2] - c2[2];
 		});
 
-		initParents(n);
-
-		int index = -1;
+		int temp = 0;
+		int index = 0;
 		int answer = 0;
 
-		while (index < costs.length-1) {
-			index++;
+		while (temp < n) {
 			int a = costs[index][0];
 			int b = costs[index][1];
 			int cost = costs[index][2];
 
-			int parentA = findParent(a);
-			int parentB = findParent(b);
-
-			if (parentA == parentB)
-				continue;
-			else {
-				if (parentA < parentB)
-					union(parentA, parentB);
+			int cnt = 0;
+			for (int i = 0; i < n; i++) {
+				if (visited[i])
+					cnt++;
 				else
-					union(parentB, parentA);
+					break;
+			}
+
+			if (cnt == n)
+				break;
+
+			if (visited[a] && visited[b])
+				index++;
+			else {
+				visited[a] = true;
+				visited[b] = true;
 				answer += cost;
+				index++;
 			}
 		}
 
 		return answer;
-	}
-
-	static public void initParents(int n) {
-		for (int i = 0; i < n; i++) {
-			parents[i] = i;
-		}
-	}
-
-	static public void union(int a, int b) {
-		int parentA = findParent(a);
-		int parentB = findParent(b);
-
-		parents[a] = parentB;
-	}
-
-	static public int findParent(int a) {
-		if (parents[a] == a)
-			return a;
-		else
-			return parents[a] = findParent(parents[a]);
 	}
 }
