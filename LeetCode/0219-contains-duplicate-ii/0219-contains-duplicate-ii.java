ac88a50039502a51275900ac5e26e0a5024db031
nayu <nyj3230@naver.com>
2023-08-30 21:59:31 +0900

    Time: 25 ms (16.33%), Space: 51.7 MB (99.88%) - LeetHub

--- LeetCode/0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ LeetCode/0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -1,14 +1,11 @@
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        HashSet<Integer> set = new HashSet<>();
-        for (int i = 0; i < nums.length; i++) {
-            if (set.contains(nums[i])) {
-                return true;
-            }
-            set.add(nums[i]);
-            if (set.size() > k) {
-                set.remove(nums[i - k]);
-            }
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i=0; i<nums.length; i++){
+            if(map.containsKey(nums[i])&& Math.abs(map.get(nums[i])-i) <= k)return true;
+            else map.put(nums[i],i);
+
+            if(i >= k && map.containsKey(nums[i-k]))map.remove(nums[i-k]);
         }
         return false;
     }
