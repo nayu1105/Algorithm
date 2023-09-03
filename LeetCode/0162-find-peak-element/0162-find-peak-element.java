12fba1f558ae1799c61bea6e7c1c79a929653e0c
nayu <nyj3230@naver.com>
2023-09-03 16:14:22 +0900

    Time: 1 ms (100%), Space: 41.3 MB (52.35%) - LeetHub

---
+++ LeetCode/0162-find-peak-element/0162-find-peak-element.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+       int index = 0;
+
+       int distance = 0;
+       int answer = 0;
+
+       if(nums.length == 1) return 0;
+       else if(nums.length == 2) return nums[0] > nums[1] ? 0 : 1;
+
+       while(index < nums.length){
+           int pre = index, before, next;
+           if(index == 0){
+               before = nums.length - 1;
+               next = index + 1;
+           }
+           
+           else if(index == nums.length - 1){
+               before = index - 1;
+               next = 0;
+           }
+           else {
+               before = index - 1;
+               next = index + 1;
+           }
+
+           if(nums[before] < nums[index] && nums[next] < nums[index]){
+               int temp = Math.abs(nums[index] - Math.max(nums[before], nums[next]));
+               if(temp > distance) answer = index;
+           }
+
+           index++;
+       }
+       
+       return answer;
+
+
+    }
+}
