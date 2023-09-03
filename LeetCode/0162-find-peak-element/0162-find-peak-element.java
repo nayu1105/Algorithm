23c504d73bce7b6861f58f2ffe0c4090613a1045
nayu <nyj3230@naver.com>
2023-09-03 16:17:02 +0900

    Time: 0 ms (100%), Space: 41.3 MB (41.51%) - LeetHub

--- LeetCode/0162-find-peak-element/0162-find-peak-element.java
+++ LeetCode/0162-find-peak-element/0162-find-peak-element.java
@@ -1,39 +1,17 @@
 class Solution {
-    public int findPeakElement(int[] nums) {
-       int index = 0;
+    public int findPeakElement(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
 
-       int distance = 0;
-       int answer = 0;
-
-       if(nums.length == 1) return 0;
-       else if(nums.length == 2) return nums[0] > nums[1] ? 0 : 1;
-
-       while(index < nums.length){
-           int pre = index, before, next;
-           if(index == 0){
-               before = nums.length - 1;
-               next = index + 1;
-           }
-           
-           else if(index == nums.length - 1){
-               before = index - 1;
-               next = 0;
-           }
-           else {
-               before = index - 1;
-               next = index + 1;
-           }
-
-           if(nums[before] < nums[index] && nums[next] < nums[index]){
-               int temp = Math.abs(nums[index] - Math.max(nums[before], nums[next]));
-               if(temp > distance) answer = index;
-           }
-
-           index++;
-       }
-       
-       return answer;
-
-
+        while(start < end){
+            int mid = start + (end - start) / 2;
+            if(arr[mid] > arr[mid + 1]){
+                end = mid;
+            }else{
+                start = mid + 1;
+            }
+        }
+        return start;
+        
     }
 }
